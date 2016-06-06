fac9ef3fc065b2520d072dae7b566ee3fec9641c
Jeni Gooley <jeni.gooley.42@gmail.com>
2016-06-06 11:41:53 -0400

    adjusted for udacity review

--- movie_project/media.py
+++ movie_project/media.py
@@ -1,10 +1,18 @@
 import webbrowser
+
+#class takes data for a movie, assigns movie data to corresponding variable and 
+#uses data to play movie trailer in browser. This is to be used for creating a 
+#simple movie website 
+
 class Movie():
+    #constructor takes information about a movie and assigns it to 
+    # corresponding variable
     def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
 
+    #opens movie trailer url in browser
     def show_trailer(self):
         webbrowser.open(self.trailer_youtube_url)
